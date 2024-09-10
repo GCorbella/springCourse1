975ff0feb69bae7ac4833d36ef90481aa677a46a
Guille P. Corbella <guillepcorbella@hotmail.com>
2024-09-10 12:38:28 +0200

    First Commit

---
+++ src/main/java/com/cursospringboot/capuno/CapunoApplication.java
@@ -0,0 +1,13 @@
+package com.cursospringboot.capuno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapunoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapunoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursospringboot/capuno/CapunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursospringboot.capuno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
